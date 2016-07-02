459bd49a22aee78539ef7c506ed5c0d423572706
136469828 <136469828@qq.com>
2016-07-02 15:33:47 +0800

    Initial Commit

---
+++ ITCradle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITCradle
+//
+//  Created by admin on 16/7/2.
+//  Copyright © 2016年 JCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ITCradle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ITCradle
+//
+//  Created by admin on 16/7/2.
+//  Copyright © 2016年 JCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
